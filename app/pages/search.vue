77cb77a8dbaa1e524704cc08dc7816c6af63135f
zhangtianxiang <547121689@qq.com>
2025-07-03 14:06:30 +0800

    ffff

--- app/pages/search.vue
+++ app/pages/search.vue
@@ -1,34 +1,16 @@
 <template>
     <div class="bg-white">
-        <div class="max-row py-8">
-            <!-- Hero Section -->
-            <div class="relative h-[300px] overflow-hidden">
-                <div class="absolute inset-0">
-                    <img src="/images/categorybanner.png" class="w-full object-cover object-top"
-                        alt="hero background" />
-                </div>
-                <div class="relative z-10 container mx-auto px-6 h-full flex flex-col justify-center"
-                    style="text-shadow: 0px 2px 4px rgba(34,34,34,0.6);">
-                    <h1 class="text-5xl font-bold text-white mb-4">Shade Solutions</h1>
-                    <p class="text-xl text-white">
-                        Transform Your Outdoor Space with Premium Sun Protection
-                    </p>
-                </div>
-            </div>
+        <div class="max-row py-6">
+
 
             <!-- Main Content -->
             <div class="container mx-auto px-6 mt-12">
                 <!-- Filters -->
-                <div class="flex justify-between items-center mb-8">
+                <div class="sticky [top:122px] bg-white z-10 flex justify-between items-center mb-8 py-4">
                     <div class="flex gap-6">
-                        <UCheckbox :checked="selected === 'Hot Selling'" @change="handleChange('Hot Selling')"
-                            label="Hot Selling" class="text-sm" />
-                        <UCheckbox :checked="selected === 'New Arrival'" @change="handleChange('New Arrival')"
-                            label="New Arrival" class="text-sm" />
-                        <UCheckbox :checked="selected === 'Discount'" @change="handleChange('Discount')"
-                            label="Discount" class="text-sm" />
+
                     </div>
-                    <div class="relative flex items-center">
+                    <div class="relative flex items-center" v-show="products.length != 0">
                         <span class="mr-4 text-sm">Sort</span>
                         <USelect size="xs" v-model="selectedsort" :options="sortarray" />
                     </div>
@@ -80,10 +62,43 @@
                             </div>
                         </div>
                     </div>
-
-                    <div class="text-center my-12" v-show="products.length === 0 && !loading">
-                        Not Found Products
+                    <div class="my-12" v-show="products.length === 0 && !loading">
+                        <div class="flex flex-col items-center text-center space-y-4 text-gray-200 text-sm">
+                            <img src="/empty.png" alt="Empty" class="w-32 h-32" />
+                            <div>
+                                <div class="mt-2">No results for “{{ keyword }}”</div>
+                                <div class="mt-2">Try checking your spelling or use more general terms</div>
+                                <div class="mt-2">Check each product page for other buying options.</div>
+                            </div>
+                        </div>
                     </div>
+
+                    <div class="text-customblack text-xl" v-show="recommendproducts.length > 0 && !loading">New Product
+                    </div>
+                    <div v-show="recommendproducts.length > 0 && !loading" class="grid grid-cols-5 gap-4 mb-12 mt-4">
+                        <div @click="checkdetail(product.id)" v-for="(product, index) in recommendproducts" :key="index"
+                            class="bg-white rounded-lg cursor-pointer">
+                            <div class="aspect-square overflow-hidden rounded-t-lg">
+                                <img :src="product.productMainPic ?? '/images/empty.jpg'"
+                                    :alt="product.productEnglishName" class="w-full h-full object-cover object-center"
+                                    style="aspect-ratio: 1 / 1;" />
+                            </div>
+                            <div>
+                                <h3 class="text-sm mb-2 text-customblack mt-3 line-clamp-2 cursor-default"
+                                    :title="product.productEnglishName">
+                                    {{ product.productEnglishName }}
+                                </h3>
+
+                                <p class="text-[#AEAEAE] mb-2 text-sm">{{ product.size }}</p>
+                                <div class="flex justify-between items-center">
+                                    <span class="text-base font-semibold">
+                                        ${{ product.customPriceStr }}
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
@@ -113,8 +128,8 @@ const selectedsort = ref(sortarray[0])
 const selected = ref('')
 const products = ref([])
 const loading = ref(false)
-
-const { getproductSearch } = ProductAuth()
+const recommendproducts = ref([])
+const { getproductSearch, productSearchRecommendation } = ProductAuth()
 const route = useRoute()
 const router = useRouter()
 const keyword = route.query.query;
@@ -143,13 +158,30 @@ const getlistlist = async () => {
 
         const res = await getproductSearch(parmes)
         products.value = res.result.list
+        if (res.result.list.length == 0) {
+            getproductSearchRecommendation()
+        }
+    } catch (error) {
+        console.error('Load product list failed:', error)
+        getproductSearchRecommendation()
+
+    } finally {
+        loading.value = false
+    }
+}
+const getproductSearchRecommendation = async () => {
+    try {
+        const parmes = {
+            lastPublishTime: 0
+        }
+        const res = await productSearchRecommendation(parmes)
+        recommendproducts.value = res.result.list
     } catch (error) {
         console.error('Load product list failed:', error)
     } finally {
         loading.value = false
     }
 }
-
 getlistlist()
 
 const checkdetail = (id) => {
